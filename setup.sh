99baab5149c7e35b8d7433acfaeb3de9ea504552
Holden Karau <holden@pigscanfly.ca>
2021-09-24 21:45:33 -0700

    I guess we are using pymesh.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -ex
+if ! command -v python &> /dev/null
+then
+  sudo apt-get install -y python
+fi
 if ! command -v npm &> /dev/null
 then
   curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
   sudo apt-get install -y nodejs
+fi
+# Install ctmconv (oldskool not so happy with obj)
+if ! command -v ctmconv &> /dev/null
+then
+  sudo apt-get install -y openctm-tools
+fi
+# Install meshlab
+if ! command -v meshlab &> /dev/null
+then
+  sudo apt-get install -y openctm-tools
 fi
 # Update npm
 sudo npm install -g npm
@@ -18,3 +32,4 @@ if [ ! -d grid-apps ]; then
 fi
 sudo make install
 popd
+pip install -U -r requirements.txt
