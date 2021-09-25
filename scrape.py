c229c94952589adc0fa730bfa65624c7f6add373
Jemma L <codejems@gmail.com>
2021-09-25 00:40:48 -0700

    Extract links
    can grab links
    - having issues with ssl certs

--- scrape.py
+++ scrape.py
@@ -1,5 +1,21 @@
 # 
-def extract_links(X):
+import requests
+from bs4 import BeautifulSoup
+def extract_links(url):
+    
+    links = []
+    f = requests.get(url)
+    links += [url, f.text]
+    # print(links)
+    soup = BeautifulSoup(f.text, 'html.parser')
+    # ssl error for me when looking at smithsonian webpage
+
+    print(soup.prettify())
+
+# grab smithsonian webpage
+extract_links('https://3d.si.edu/explore')
+
+
     """Get all of the links for page X of the smithstonian results."""
     # They live at https://3d.si.edu/explore?page=X
 
@@ -19,3 +35,4 @@ valid_first_page_links = list(filter(is_3d_model, first_page_links))
 # Some details: the smithstonian website loops infinitely (e.g. ask for page 100000 and it will always give you some data back).
 while True:
     # Break 
+
