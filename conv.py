b0b89043d1f113827e91eb307320afe8faf9e0cd
Holden Karau <holden@pigscanfly.ca>
2021-09-25 19:27:01 -0700

    Automatically scale everything

--- conv.py
+++ conv.py
@@ -8,5 +8,9 @@ if len(sys.argv) != 2:
 input_file = sys.argv[1]
 ms = ml.MeshSet()
 ms.load_new_mesh(input_file)
+# Make it fit
+biggest = max(ms.current_mesh().bounding_box().dim_y(), ms.current_mesh().bounding_box().dim_x(), ms.current_mesh().bounding_box().dim_z())
+scale = 150 / biggest
+ms.apply_filter('matrix_set_from_translation_rotation_scale', scalex=scale, scaley=scale, scalez=scale)
 ms.save_current_mesh(f"{input_file}.stl")
 sys.exit(0)
