02c1085a4dba43b4990a1a5cb96df16e99f6e645
Holden Karau <holden@pigscanfly.ca>
2021-10-24 12:21:21 -0700

    Update link extraction

--- scrape.py
+++ scrape.py
@@ -2,22 +2,26 @@
 import requests
 from bs4 import BeautifulSoup
 def extract_links(url):
-    
     links = []
     f = requests.get(url)
-    links += [url, f.text]
     # print(links)
     soup = BeautifulSoup(f.text, 'html.parser')
-    # ssl error for me when looking at smithsonian webpage
+    links = soup.find_all('a')
+    urls = []
+    for l in links:
+        try:
+            urls.append(l["href"])
+        except:
+            pass
+    return urls
 
-    print(soup.prettify())
 
-# grab smithsonian webpage
-extract_links('https://3d.si.edu/explore')
-
-
+def links_for_page(id):
     """Get all of the links for page X of the smithstonian results."""
     # They live at https://3d.si.edu/explore?page=X
+    # grab smithsonian webpage
+    return extract_links(f'https://3d.si.edu/explore?page={id}')
+
 
 def is_3d_model(url):
     """Is the link (probably) to a 3D model?"""
@@ -30,9 +34,11 @@ def can_slice(url):
     """Is there anything at the link we can slice."""
 
 
-first_page_links = extract_links(1)
+first_page_links = links_for_page(1)
+print(first_page_links)
 valid_first_page_links = list(filter(is_3d_model, first_page_links))
 # Some details: the smithstonian website loops infinitely (e.g. ask for page 100000 and it will always give you some data back).
-while True:
-    # Break 
-
+page = 2
+#while True:
+#    page = page + 1
+#     new_links = list(filter(is_3d_model, links_for_page(page)))
