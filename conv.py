82348a57e4c0e9739adb4ad6d9cc172046c2fa0d
Holden Karau <holden@pigscanfly.ca>
2021-10-24 21:38:31 -0700

    Ok 90 degress should do the trick

--- conv.py
+++ conv.py
@@ -12,5 +12,6 @@ ms.load_new_mesh(input_file)
 biggest = max(ms.current_mesh().bounding_box().dim_y(), ms.current_mesh().bounding_box().dim_x(), ms.current_mesh().bounding_box().dim_z())
 scale = 100 / biggest
 ms.apply_filter('matrix_set_from_translation_rotation_scale', scalex=scale, scaley=scale, scalez=scale)
+ms.apply_filter('transform_rotate', rotaxis='X axis', angle=90)
 ms.save_current_mesh(f"{input_file}.stl")
 sys.exit(0)
